42f04fb38fbaed90967bbc1ee6aea76f307fd2a3
daczarne <daniel.czarnievicz@gmail.com>
2018-11-01 14:33:32 -0300

    agrega 1 y 2 kms

--- Distance between supers/Distance_between_Supers.R
+++ Distance between supers/Distance_between_Supers.R
@@ -32,8 +32,8 @@ dst[lower.tri(dst)] <- NA
 ###################################################
 
 # Calculates which supers are at x or less distance from each other. Different values of x are implemented.
-# For each distance value creates a vector. Each vector value represents the amount of supermarkets at less than xKms from a given supermarket
-# This is not the relevant question for this study
+# For each distance value creates a vector. Each vector value represents the amount of supermarkets at less
+# than xKms from a given supermarket. This is not the relevant question for this study
 
 # calculates how many supers each super has at a distance less than ...
 less.than.5k <- apply(dst, 1, function(x) length(which(x <= 0.5))) # 500 meters
@@ -46,37 +46,43 @@ y <- as.data.frame(cbind(x, less.than.5k, less.than.1k, less.than.15k, less.than
 #### PART 3 - Distance to Tata ####
 ###################################
 
-# Calculates which supers have a supermarket of chain Tata at less than 0.5Km
+# Calculates which stores have a supermarket of chain Tata at less than 0.5, 1 and 2Km
 # Will create a data.frame:
 #     variable 1: supermarket number
-#     variable 2: logical. TRUE=super has a Tata at less than 0.5Km
+#     variable 2: logical. TRUE = store has a Tata at less than 0.5 Km
+#     variable 3: logical. TRUE = store has a Tata at less than 1 km
+#     variable 4: logical. TRUE = store has a Tata at less than 2 km 
 
-# 1) Computes an NxN matrix, Each cell takes a TRUE value if the two supers are at a distance less than 500 meters.
+# For each distance:
+# 1) Computes an NxN matrix, Each cell takes a TRUE value if the two stores are at a 
+# distance less than 500 meters.
 # 2) Reshapes the matrix into a data frame that contains chain names.
-# 3) Checks for lines where Chain2 is Tata and Chain1 and Chain2 are at less than 500 meters
+# 3) Checks for lines where Chain2 is Tata and Chain1 and Chain2 are at less than 500 mts
 # 4) Groups by Super1
-# 5) Computes the summation of all flags from step 3), if summation is greater than zero (that is, if there's at least one TRUE), it flags it as TRUE, otherwise, as FALSE
-# Result is a dataframe which lists all stores by number and name and flags (TRUE) which of them have a tata store less than 500 meters away.
-menor.500 <- (dst <= 0.5) %>% 
-      reshape2::melt(na.rm=TRUE) %>% 
-      as_tibble() %>% 
+# 5) Computes the summation of all flags from step 3), if summation is greater than 
+# zero (that is, if there's at least one TRUE), it flags it as TRUE, otherwise, is FALSE
+# Result is a dataframe which lists all stores by number and name and flags (TRUE) which
+# of them have a tata store less than 500 meters away.
+menor <- (dst <= 0.5) %>%
+      reshape2::melt(na.rm=TRUE) %>%
+      as_tibble() %>%
       dplyr::rename(Super1 = Var1, Super2 = Var2, Less = value) %>%
-      left_join(dplyr::select(Establecimientos, Super, chain), by=c("Super1" = "Super")) %>% 
+      left_join(dplyr::select(Establecimientos, Super, chain), by=c("Super1" = "Super")) %>%
       dplyr::rename(Chain1 = chain) %>%
-      left_join(dplyr::select(Establecimientos, Super, chain), by=c("Super2" = "Super")) %>% 
+      left_join(dplyr::select(Establecimientos, Super, chain), by=c("Super2" = "Super")) %>%
       dplyr::rename(Chain2 = chain) %>%
       dplyr::select(Super1, Chain1, Super2, Chain2, Less) %>%
       dplyr::arrange(Super1) %>%
       mutate(haytata = if_else((Chain2 == "Ta - Ta") & (Less == TRUE), TRUE, FALSE)) %>%
       group_by(Super1, Chain1) %>%
-      summarise(tata.menos.500 = if_else(sum(haytata) > 0, TRUE, FALSE))
-super.718 <- (dst <= 0.5) %>% 
-      reshape2::melt(na.rm=TRUE) %>% 
-      as_tibble() %>% 
+      summarise(menor.500 = if_else(sum(haytata) > 0, TRUE, FALSE))
+super.718 <- (dst <= 0.5) %>%
+      reshape2::melt(na.rm=TRUE) %>%
+      as_tibble() %>%
       dplyr::rename(Super1 = Var1, Super2 = Var2, Less = value) %>%
-      left_join(dplyr::select(Establecimientos, Super, chain), by=c("Super1" = "Super")) %>% 
+      left_join(dplyr::select(Establecimientos, Super, chain), by=c("Super1" = "Super")) %>%
       dplyr::rename(Chain1 = chain) %>%
-      left_join(dplyr::select(Establecimientos, Super, chain), by=c("Super2" = "Super")) %>% 
+      left_join(dplyr::select(Establecimientos, Super, chain), by=c("Super2" = "Super")) %>%
       dplyr::rename(Chain2 = chain) %>%
       dplyr::select(Super1, Chain1, Super2, Chain2, Less) %>%
       dplyr::arrange(Super1) %>%
@@ -85,11 +91,77 @@ super.718 <- (dst <= 0.5) %>%
       table(.$haytata) %>%
       names() %>%
       as.logical()
-menor.500[(dim(menor.500)[1]+1),] <- c(718, as.character(Establecimientos[Establecimientos$Super == 718L,]$chain), super.718)
-menor.500$Super1 <- as.numeric(menor.500$Super1)
-Establecimientos <- left_join(Establecimientos, menor.500, by=c("Super" = "Super1", "chain" = "Chain1"))
+menor[(dim(menor)[1]+1),] <- c(718, as.character(Establecimientos[Establecimientos$Super == 718L,]$chain), super.718)
+menor$Super1 <- as.numeric(menor$Super1)
+Establecimientos <- left_join(Establecimientos, menor, by=c("Super" = "Super1", "chain" = "Chain1"))
 
-# agregar 1km y 2km
+# Repeats the process for 1km
+menor <- (dst <= 1) %>%
+      reshape2::melt(na.rm=TRUE) %>%
+      as_tibble() %>%
+      dplyr::rename(Super1 = Var1, Super2 = Var2, Less = value) %>%
+      left_join(dplyr::select(Establecimientos, Super, chain), by=c("Super1" = "Super")) %>%
+      dplyr::rename(Chain1 = chain) %>%
+      left_join(dplyr::select(Establecimientos, Super, chain), by=c("Super2" = "Super")) %>%
+      dplyr::rename(Chain2 = chain) %>%
+      dplyr::select(Super1, Chain1, Super2, Chain2, Less) %>%
+      dplyr::arrange(Super1) %>%
+      mutate(haytata = if_else((Chain2 == "Ta - Ta") & (Less == TRUE), TRUE, FALSE)) %>%
+      group_by(Super1, Chain1) %>%
+      summarise(menor.1 = if_else(sum(haytata) > 0, TRUE, FALSE))
+super.718 <- (dst <= 1) %>%
+      reshape2::melt(na.rm=TRUE) %>%
+      as_tibble() %>%
+      dplyr::rename(Super1 = Var1, Super2 = Var2, Less = value) %>%
+      left_join(dplyr::select(Establecimientos, Super, chain), by=c("Super1" = "Super")) %>%
+      dplyr::rename(Chain1 = chain) %>%
+      left_join(dplyr::select(Establecimientos, Super, chain), by=c("Super2" = "Super")) %>%
+      dplyr::rename(Chain2 = chain) %>%
+      dplyr::select(Super1, Chain1, Super2, Chain2, Less) %>%
+      dplyr::arrange(Super1) %>%
+      mutate(haytata = if_else((Chain2 == "Ta - Ta") & (Less == TRUE), TRUE, FALSE)) %>%
+      filter(Super2 == 718) %$%
+      table(.$haytata) %>%
+      names() %>%
+      as.logical()
+menor[(dim(menor)[1]+1),] <- c(718, as.character(Establecimientos[Establecimientos$Super == 718L,]$chain), super.718)
+menor$Super1 <- as.numeric(menor$Super1)
+Establecimientos <- left_join(Establecimientos, menor, by=c("Super" = "Super1", "chain" = "Chain1"))
+
+# Repeats the process for 2kms
+menor <- (dst <= 2) %>%
+      reshape2::melt(na.rm=TRUE) %>%
+      as_tibble() %>%
+      dplyr::rename(Super1 = Var1, Super2 = Var2, Less = value) %>%
+      left_join(dplyr::select(Establecimientos, Super, chain), by=c("Super1" = "Super")) %>%
+      dplyr::rename(Chain1 = chain) %>%
+      left_join(dplyr::select(Establecimientos, Super, chain), by=c("Super2" = "Super")) %>%
+      dplyr::rename(Chain2 = chain) %>%
+      dplyr::select(Super1, Chain1, Super2, Chain2, Less) %>%
+      dplyr::arrange(Super1) %>%
+      mutate(haytata = if_else((Chain2 == "Ta - Ta") & (Less == TRUE), TRUE, FALSE)) %>%
+      group_by(Super1, Chain1) %>%
+      summarise(menor.2 = if_else(sum(haytata) > 0, TRUE, FALSE))
+super.718 <- (dst <= 2) %>%
+      reshape2::melt(na.rm=TRUE) %>%
+      as_tibble() %>%
+      dplyr::rename(Super1 = Var1, Super2 = Var2, Less = value) %>%
+      left_join(dplyr::select(Establecimientos, Super, chain), by=c("Super1" = "Super")) %>%
+      dplyr::rename(Chain1 = chain) %>%
+      left_join(dplyr::select(Establecimientos, Super, chain), by=c("Super2" = "Super")) %>%
+      dplyr::rename(Chain2 = chain) %>%
+      dplyr::select(Super1, Chain1, Super2, Chain2, Less) %>%
+      dplyr::arrange(Super1) %>%
+      mutate(haytata = if_else((Chain2 == "Ta - Ta") & (Less == TRUE), TRUE, FALSE)) %>%
+      filter(Super2 == 718) %$%
+      table(.$haytata) %>%
+      names() %>%
+      as.logical()
+menor[(dim(menor)[1]+1),] <- c(718, as.character(Establecimientos[Establecimientos$Super == 718L,]$chain), super.718)
+menor$Super1 <- as.numeric(menor$Super1)
+Establecimientos <- left_join(Establecimientos, menor, by=c("Super" = "Super1", "chain" = "Chain1"))
+
+save(Establecimientos, file="Establecimientos.RData")
 
 ############################
 #### END OF PROGRAMMING ####
