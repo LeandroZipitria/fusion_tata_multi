32ca619485c55cbe3f4251ecd2ab24f58d3a2de7
daczarne <daniel.czarnievicz@gmail.com>
2018-11-06 17:33:19 -0300

    filtra supers en funcion de prods

--- estad_descript.R
+++ estad_descript.R
@@ -5,13 +5,44 @@
 library(tidyverse)
 library(magrittr)
 
+#########################
+#### PRODS POR SUPER ####
+#########################
+
+# Número de productos
+# Creates a unique data.frame with all the data by super, prod, year and month
+prods <- readr::read_csv("Bases TATA/P_All_25_clean.csv")
+prods <- prods %>%
+      filter(!is.na(Price)) %>%
+      group_by(Product, Year, Month, Super) %>%
+      summarise(obs = n(), 
+                moda = as.numeric(names(sort(table(Price), decreasing=TRUE)))[1L])
+      
+for (i in seq(50, 150, 25)) {
+      path <- paste0("Bases TATA/P_All_", i, "_clean.csv")
+      prods <- readr::read_csv(file=path) %>%
+            filter(!is.na(Price)) %>%
+            group_by(Product, Year, Month, Super) %>%
+            summarise(obs = n(), 
+                      moda = as.numeric(names(sort(table(Price), decreasing=T)))[1L]) %>%
+            bind_rows(prods, .)
+}
+
+# Number of goods per store
+num_prods <- prods %>%
+      group_by(Super) %>%
+      summarise(obs = n(),
+                num_prods = length(unique(Product)))
+
 ################
 #### STORES ####
 ################
 
-stores <- read_rds("Establecimientos.rds")
-
 #### SOLAPAMIENTO DE TIENDAS ####
+
+# Los supermercados para los que no hay productos en la base de prods son descartados
+stores <- read_rds("Establecimientos.rds")
+stores <- filter(stores, Super %in% unique(num_prods$Super)) %>% droplevels()
 
 # No existen Multiahorros con un TaTa a 500 mts o menos
 stores %>%
@@ -43,44 +74,6 @@ stores %>%
       mutate(interior = if_else(depto != "Montevideo", 1, 0)) %>%
       group_by(chain) %>%
       summarise(interior = sum(interior), montevideo = n() - interior)
-
-#### Número de productos ####
-prods <- readr::read_csv("Bases TATA/P_All_25_clean.csv")
-prods <- prods %>%
-      filter(!is.na(Price)) %>%
-      group_by(Product, Year, Month, Super) %>%
-      summarise(obs = n(), 
-                moda = as.numeric(names(sort(table(Price), decreasing=TRUE)))[1L])
-      
-for (i in seq(50, 150, 25)) {
-      path <- paste0("Bases TATA/P_All_", i, "_clean.csv")
-      prods <- readr::read_csv(file=path) %>%
-            filter(!is.na(Price)) %>%
-            group_by(Product, Year, Month, Super) %>%
-            summarise(obs = n(), 
-                      moda = as.numeric(names(sort(table(Price), decreasing=T)))[1L]) %>%
-            bind_rows(prods, .)
-}
-
-prods %>%
-      group_by(Super) %>%
-      summarise(obs = n(),
-                num_prods = length(unique(Product)))
-
-
-# Separar muestra en dos:
-#     más de un km 
-#     menos de un km
-
-# tamaño promedio del super (en cajas) 
-# super location
-
-# Tratamiento; ser otro super que está a menos de un km de un tata
-
-
-# feb 2010 - feb 2013 (fusión) - feb 2016
-# feb 2009 - feb 2013 (fusión) - feb 2017
-
 
 #################################
 #### FIN DE LA PROGRAMAACIÓN ####
