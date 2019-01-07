606176279a2d1ee15c30e51e09536ca86a1b905b
daczarne <daniel.czarnievicz@gmail.com>
2019-01-07 16:50:53 -0300

    the graph is just for myself :)

--- 4_analisis.R
+++ 4_analisis.R
@@ -6,6 +6,7 @@ library(tidyverse)
 library(magrittr)
 library(lubridate)
 library(lfe)
+library(ggthemes)
 
 # Carga la base
 prods <- readr::read_rds(path = "precios_mensuales.rds")
@@ -17,17 +18,26 @@ prods <- mutate(prods,
        Day = 1L,
        Date = lubridate::make_date(year = Year, month = Month, day = Day),
        T_i = if_else(Date < ymd("2013-02-01"), 0, 1)) %>%
-      select(-Day)
+      select(-Day) %>%
+      filter(is.na(menor.500) == FALSE)
 
-prods %>%
-      ungroup() %>%
-      filter(Product %in% c(1:9)) %>%
-      dplyr::select(Product, Date, moda, menor.500) %>%
+ungroup(prods) %>%
+      filter(Product %in% c(1:4)) %>%
+      dplyr::select(Product, Date, moda, menor.500, Super) %>%
       group_by(Product, Date, menor.500) %>%
       summarise(promedio = mean(moda)) %>%
       ggplot() +
-      geom_line(aes(Date, promedio)) +
-      facet_wrap(~Product)
+      geom_line(aes(Date, promedio, color = as.factor(menor.500))) +
+      geom_vline(aes(xintercept = make_date(year = 2013L, month = 2L, day = 1L)),
+                     linetype = 2) +
+      facet_wrap(~ as.factor(Product), scales = "free_y") +
+      labs(x = NULL, y = "Mode mean", color = NULL) +
+      scale_color_discrete(labels = c("Control","Treatment")) +
+      ggthemes::theme_economist() +
+      theme(legend.position = "bottom")
+
+summary(lm(moda ~ menor.500*T_i, data = prods))
+
 
 ################################
 #### FIN DE LA PROGRAMACIÓN ####
