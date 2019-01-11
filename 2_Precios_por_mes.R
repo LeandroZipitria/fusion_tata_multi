0eb8dd894c40c0433e929cadbc7178a36857d546
daczarne <daniel.czarnievicz@gmail.com>
2019-01-11 16:01:26 -0300

    ungrouped tibble

--- 2_Precios_por_mes.R
+++ 2_Precios_por_mes.R
@@ -11,24 +11,25 @@ prods <- prods %>%
       filter(!is.na(Price)) %>%
       group_by(Product, Year, Month, Super) %>%
       summarise(obs = n(), 
-                moda = as.numeric(names(sort(table(Price), decreasing=T)))[1L])
+                moda = as.numeric(names(sort(table(Price), decreasing = T)))[1L])
 
 for (i in seq(50, 150, 25)) {
       path <- paste0("Bases TATA/P_All_", i, "_clean.csv")
-      prods <- read.csv(file=path) %>%
+      prods <- read.csv(file = path) %>%
             as_tibble() %>%
             filter(!is.na(Price)) %>%
             group_by(Product, Year, Month, Super) %>%
             summarise(obs = n(), 
-                      moda = as.numeric(names(sort(table(Price), decreasing=T)))[1L]) %>%
+                      moda = as.numeric(names(sort(table(Price), decreasing = T)))[1L]) %>%
             bind_rows(prods, .)
 }
 
-stores <- read_rds("Establecimientos.rds")
-stores <- select(stores, Super, starts_with("menor."))
-prods <- left_join(prods, stores, by="Super")
+prods <- ungroup(prods)
+stores <- read_rds("Establecimientos.rds") %>%
+      select(Super, starts_with("menor."), chain)
+prods <- left_join(prods, stores, by = "Super")
 
-readr::write_rds(prods, path="precios_mensuales.rds")
+readr::write_rds(prods, path = "precios_mensuales.rds")
 
 #################################
 #### FIN DE LA PROGRAMAACIÓN ####
--- 4_analisis.R
+++ 4_analisis.R
@@ -2,28 +2,39 @@
 #### ANÁLISIS DE PRECIOS ####
 #############################
 
+library(caret)
+library(lubridate)
 library(tidyverse)
 library(magrittr)
-library(lubridate)
 library(lfe)
 library(ggthemes)
 
 # Carga la base
-prods <- readr::read_rds(path = "precios_mensuales.rds")
+prods <- readr::read_rds(path = "precios_mensuales.rds") %>%
+      filter(is.na(menor.500) == FALSE)
 
 # Crea variable T_i la cual tomoa valor:
 #     0 si la fecha de reporte es anterior a la fusión
 #     1 si la fecha de reporte es posterior a la fusión
-prods <- mutate(prods, 
-       Day = 1L,
-       Date = lubridate::make_date(year = Year, month = Month, day = Day),
-       T_i = if_else(Date < ymd("2013-02-01"), 0, 1)) %>%
-      select(-Day) %>%
-      filter(is.na(menor.500) == FALSE)
+prods <- mutate(
+      prods,
+      Product = as.factor(Product),
+      Day = 1L,
+      Date = lubridate::make_date(year = Year, month = Month, day = Day),
+      P_t = if_else(Date < ymd("2013-02-01"), 0, 1),
+      T_i = if_else(chain == "Ta - Ta" | chain == "Multi Ahorro", 1, 0)) %>%
+      select(-Day)
+
+dummies <- predict(dummyVars(~ as.character(Product), data = prods), newdata = prods)
+head(dummies)
+
+summary(lm(moda ~ Product + Date, data = prods))
+felm(moda ~ P_t + | Product + Date)
+
 
 ungroup(prods) %>%
-      filter(Product %in% c(1:4)) %>%
-      dplyr::select(Product, Date, moda, menor.500, Super) %>%
+      filter(Product %in% sample(unique(prods$Product), 4)) %>%
+      dplyr::select(Product, Super, Date, moda, menor.500) %>%
       group_by(Product, Date, menor.500) %>%
       summarise(promedio = mean(moda)) %>%
       ggplot() +
@@ -31,13 +42,45 @@ ungroup(prods) %>%
       geom_vline(aes(xintercept = make_date(year = 2013L, month = 2L, day = 1L)),
                      linetype = 2) +
       facet_wrap(~ as.factor(Product), scales = "free_y") +
-      labs(x = NULL, y = "Mode mean", color = NULL) +
+      labs(x = NULL, y = "Mean mode", color = NULL) +
       scale_color_discrete(labels = c("Control","Treatment")) +
       ggthemes::theme_economist() +
-      theme(legend.position = "bottom")
+      theme(legend.position = "bottom",
+            axis.title = element_text(face = "bold"))
 
-summary(lm(moda ~ menor.500*T_i, data = prods))
+a <- felm(moda ~ P_t*T_i + menor.500, data = prods)
+summary(a)
 
+
+
+##### felm example
+
+oldopts <- options(lfe.threads = 1)
+## create covariates
+x <- rnorm(1000)
+x2 <- rnorm(length(x))
+
+## individual and firm
+id <- factor(sample(20, length(x), replace = TRUE))
+firm <- factor(sample(13, length(x), replace = TRUE))
+
+## effects for them
+id.eff <- rnorm(nlevels(id))
+firm.eff <- rnorm(nlevels(firm))
+
+## left hand side
+u <- rnorm(length(x))
+y <- x + 0.5*x2 + id.eff[id] + firm.eff[firm] + u
+
+## estimate and print result
+summary(felm(y ~ x + x2 | id + firm))
+summary(lm(y ~ x + x2 + id + firm - 1))
+
+
+library(foreign)
+mydata = read.dta("http://dss.princeton.edu/training/Panel101.dta")
+ggplot(mydata) +
+      geom_line(aes(make_date(year, 1L, 1L), y, color = country))
 
 ################################
 #### FIN DE LA PROGRAMACIÓN ####
