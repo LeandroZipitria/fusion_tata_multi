794910eb68883e01490b677b1a9e195359a4920f
daczarne <daniel.czarnievicz@gmail.com>
2019-01-15 11:00:03 -0300

    corrección de comentarios

--- 4_analisis.R
+++ 4_analisis.R
@@ -9,13 +9,17 @@ library(magrittr)
 library(lfe)
 library(ggthemes)
 
-# Carga la base
+# Carga la base de productos
 prods <- readr::read_rds(path = "precios_mensuales.rds") %>%
       filter(is.na(menor.500) == FALSE)
 
-# Crea variable T_i la cual tomoa valor:
+# Crea variable P_t la cual tomoa valor:
 #     0 si la fecha de reporte es anterior a la fusión
 #     1 si la fecha de reporte es posterior a la fusión
+# Crea variable T_i la cual tomoa valor:
+#     0 si la cadena no es Tata o Multiahorro
+#     1 si la cadena es Tata o Multiahorro
+# Convierte Product en un factor y genera la variable Date
 prods <- mutate(
       prods,
       Product = as.factor(Product),
@@ -25,20 +29,19 @@ prods <- mutate(
       T_i = if_else(chain == "Ta - Ta" | chain == "Multi Ahorro", 1, 0)) %>%
       select(-Day)
 
-dummies <- predict(dummyVars(~ as.character(Product), data = prods), newdata = prods)
-head(dummies)
-
+# No alcanza la memoria
+# dummies <- predict(dummyVars(~ as.character(Product), data = prods), newdata = prods)
+# head(dummies)
 summary(lm(moda ~ Product + Date, data = prods))
-felm(moda ~ P_t + | Product + Date)
-
+summary(felm(moda ~ Product + P_t*T_i | Product, data = prods))
 
 ungroup(prods) %>%
       filter(Product %in% sample(unique(prods$Product), 4)) %>%
-      dplyr::select(Product, Super, Date, moda, menor.500) %>%
-      group_by(Product, Date, menor.500) %>%
+      dplyr::select(Product, Super, Date, moda, menor.500, T_i) %>%
+      group_by(Product, Date, T_i) %>%
       summarise(promedio = mean(moda)) %>%
       ggplot() +
-      geom_line(aes(Date, promedio, color = as.factor(menor.500))) +
+      geom_line(aes(Date, promedio, color = as.factor(T_i))) +
       geom_vline(aes(xintercept = make_date(year = 2013L, month = 2L, day = 1L)),
                      linetype = 2) +
       facet_wrap(~ as.factor(Product), scales = "free_y") +
@@ -47,40 +50,6 @@ ungroup(prods) %>%
       ggthemes::theme_economist() +
       theme(legend.position = "bottom",
             axis.title = element_text(face = "bold"))
-
-a <- felm(moda ~ P_t*T_i + menor.500, data = prods)
-summary(a)
-
-
-
-##### felm example
-
-oldopts <- options(lfe.threads = 1)
-## create covariates
-x <- rnorm(1000)
-x2 <- rnorm(length(x))
-
-## individual and firm
-id <- factor(sample(20, length(x), replace = TRUE))
-firm <- factor(sample(13, length(x), replace = TRUE))
-
-## effects for them
-id.eff <- rnorm(nlevels(id))
-firm.eff <- rnorm(nlevels(firm))
-
-## left hand side
-u <- rnorm(length(x))
-y <- x + 0.5*x2 + id.eff[id] + firm.eff[firm] + u
-
-## estimate and print result
-summary(felm(y ~ x + x2 | id + firm))
-summary(lm(y ~ x + x2 + id + firm - 1))
-
-
-library(foreign)
-mydata = read.dta("http://dss.princeton.edu/training/Panel101.dta")
-ggplot(mydata) +
-      geom_line(aes(make_date(year, 1L, 1L), y, color = country))
 
 ################################
 #### FIN DE LA PROGRAMACIÓN ####
