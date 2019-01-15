9fd6eb0a1890d657df66a4ceaffce11135035da3
daczarne <daniel.czarnievicz@gmail.com>
2019-01-15 10:54:18 -0300

    agrega ingresos medios por depto y ccz

--- 2_Precios_por_mes.R
+++ 2_Precios_por_mes.R
@@ -4,6 +4,7 @@
 
 library(tidyverse)
 library(magrittr)
+library(foreign)
 
 # Creates a unique data.frame with all the data by super, prod, year and month
 prods <- read.csv("Bases TATA/P_All_25_clean.csv") %>% as_tibble()
@@ -25,9 +26,20 @@ for (i in seq(50, 150, 25)) {
 }
 
 prods <- ungroup(prods)
-stores <- read_rds("Establecimientos.rds") %>%
-      select(Super, starts_with("menor."), chain)
+stores <- read_rds("Establecimientos.rds")
 prods <- left_join(prods, stores, by = "Super")
+
+# Carga la base de ingresos medios
+ingmed <- read.dta("Ingreso medio 6ta reunion.dta") %>% as_tibble()
+ingmed <- ingmed %>%
+      rename(depto = departamento) %>%
+      mutate(depto = str_to_title(depto),
+             depto = if_else(depto == "Paysandu", "Paysandú",
+                     if_else(depto == "Río Negro", "Río Negro",
+                     if_else(depto == "San Jose", "San José",
+                     if_else(depto == "Tacuarembo", "Tacuarembó", depto)))))
+prods <- left_join(prods, ingmed, by = c("Year", "Month", "depto", "ccz")) %>%
+      select(Year, Month, depto, ccz, ingresomedio, Product, everything())
 
 readr::write_rds(prods, path = "precios_mensuales.rds")
 
