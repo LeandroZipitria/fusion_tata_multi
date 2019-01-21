6c13bb749c4d853fe3505c4772073f43d7cf31a2
daczarne <daniel.czarnievicz@gmail.com>
2019-01-21 14:36:12 -0300

    did and did2 (not sure which one to use)

--- 4_analisis.R
+++ 4_analisis.R
@@ -13,35 +13,43 @@ library(ggthemes)
 prods <- readr::read_rds(path = "precios_mensuales.rds") %>%
       filter(is.na(menor.500) == FALSE)
 
-# Crea variable P_t la cual tomoa valor:
+# Convierte Product en un factor 
+# Genera la variable Date
+# Crea variable P_t la cual toma valor:
 #     0 si la fecha de reporte es anterior a la fusión
 #     1 si la fecha de reporte es posterior a la fusión
-# Crea variable T_i la cual tomoa valor:
-#     0 si la cadena no es Tata o Multiahorro
-#     1 si la cadena es Tata o Multiahorro
-# Convierte Product en un factor y genera la variable Date
 prods <- mutate(
       prods,
       Product = as.factor(Product),
       Day = 1L,
       Date = lubridate::make_date(year = Year, month = Month, day = Day),
-      P_t = if_else(Date < ymd("2013-02-01"), 0, 1),
-      T_i = if_else(chain == "Ta - Ta" | chain == "Multi Ahorro", 1, 0)) %>%
-      select(-Day)
+      P_t = if_else(Date < ymd("2013-02-01"), 0, 1)) %>%
+      dplyr::select(-Day)
 
 # No alcanza la memoria
 # dummies <- predict(dummyVars(~ as.character(Product), data = prods), newdata = prods)
 # head(dummies)
-summary(lm(moda ~ Product + Date, data = prods))
-summary(felm(moda ~ Product + P_t*T_i | Product, data = prods))
+# summary(lm(moda ~ Product + Date, data = prods))
+did <- felm(moda ~ Product + P_t*menor.500 + ingresomedio | Product, data = prods)
+summary(did)
+
+prods <- prods %>%
+      count(Year, Month) %>%
+      mutate(trend = seq(1, 123, 1)) %>%
+      dplyr::select(-n) %>%
+      right_join(prods, trend, by = c("Year", "Month")) %>%
+      select(Year, Month, trend, everything())
+
+did2 <- felm(moda ~ Product + trend + P_t*menor.500 + ingresomedio | Product, data = prods)
+summary(did2)
 
 ungroup(prods) %>%
       filter(Product %in% sample(unique(prods$Product), 4)) %>%
-      dplyr::select(Product, Super, Date, moda, menor.500, T_i) %>%
-      group_by(Product, Date, T_i) %>%
+      dplyr::select(Product, Super, Date, moda, menor.500) %>%
+      group_by(Product, Date, menor.500) %>%
       summarise(promedio = mean(moda)) %>%
       ggplot() +
-      geom_line(aes(Date, promedio, color = as.factor(T_i))) +
+      geom_line(aes(Date, promedio, color = menor.500)) +
       geom_vline(aes(xintercept = make_date(year = 2013L, month = 2L, day = 1L)),
                      linetype = 2) +
       facet_wrap(~ as.factor(Product), scales = "free_y") +
